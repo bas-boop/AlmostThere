fe2538d217ae8131370c339eb9539e946a96e554
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-02-23 14:16:18 +0100

    Added bike prototype

--- Assets/Scripts/Player/Movement/Walking.cs
+++ Assets/Scripts/Player/Movement/Walking.cs
@@ -5,7 +5,9 @@ namespace Player.Movement
     [RequireComponent(typeof(Rigidbody))]
     public sealed class Walking : MonoBehaviour
     {
-        [SerializeField] private float speed = 1;
+        [SerializeField] public float speed = 1;
+        [SerializeField] private float rotationSpeed = 1;
+        [SerializeField] private Transform bike;
         
         private Vector2 _moveDirection;
         private Rigidbody _rigidbody;
@@ -23,7 +25,7 @@ namespace Player.Movement
         private void Move()
         {
             Quaternion yRotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
-            Vector3 localMove = new (_moveDirection.x, 0, _moveDirection.y);
+            Vector3 localMove = new(_moveDirection.x, 0, _moveDirection.y);
             Vector3 worldMove = yRotation * localMove;
 
             Vector3 newVelocity = _rigidbody.linearVelocity;
@@ -31,6 +33,12 @@ namespace Player.Movement
             newVelocity.z = worldMove.z * speed;
 
             _rigidbody.linearVelocity = newVelocity;
+
+            if (worldMove.sqrMagnitude <= 0.001f)
+                return;
+            
+            Quaternion targetRotation = Quaternion.LookRotation(worldMove);
+            bike.rotation = Quaternion.Slerp(bike.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
         }
     }
 }
