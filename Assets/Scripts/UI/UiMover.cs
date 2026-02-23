870a42ef93629a782ec70538ba09866c17b92ca7
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-02-23 14:54:06 +0100

    Added map with player indicator

---
+++ Assets/Scripts/UI/UiMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public sealed class UiMover : MonoBehaviour
+    {
+        [SerializeField] private Transform followTarget;
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private RectTransform mapRect; // reference to the map UI element
+        [SerializeField] private Vector2 offset;
+        [SerializeField] private float moveScale = 1;
+
+        private void Update()
+        {
+            Vector2 worldPos = new Vector2(followTarget.position.x, followTarget.position.z);
+            rectTransform.SetParent(mapRect); // ensure it's a child of the map
+            rectTransform.anchoredPosition = worldPos * moveScale + offset;
+        }
+    }
+}
