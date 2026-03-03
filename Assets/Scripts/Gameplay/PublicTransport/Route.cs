e908cbbae007170dfb006843137220e2a59d57b5
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-03-03 20:36:55 +0100

    Added transit vechile that moves via waypoints

---
+++ Assets/Scripts/Gameplay/PublicTransport/Route.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.PublicTransport
+{
+    public class Route : MonoBehaviour
+    {
+        [SerializeField] private List<Stop> stops;
+
+        private int _currentStop;
+        
+        public Vector3 GetNextStopLocation()
+        {
+            _currentStop++;
+            return stops[(_currentStop % stops.Count + stops.Count) % stops.Count].transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/PublicTransport/TransitVehicle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+using Framework.Extensions;
+
+namespace Gameplay.PublicTransport
+{
+    public class TransitVehicle : MonoBehaviour
+    {
+        [SerializeField] private Route route;
+        [SerializeField] private float speed = 1;
+
+        private Vector3 _lastStop;
+        private Vector3 _currentStop;
+
+        private void Update()
+        {
+            if (transform.position.IsWithinRange(_currentStop, 0.1f))
+            {
+                UpdateCurrentStop();
+            }
+            
+            Vector3 dir = _currentStop - transform.position;
+            transform.Translate(dir.normalized * (speed * Time.deltaTime), Space.World);
+        }
+
+        private void UpdateCurrentStop()
+        {
+            _lastStop = _currentStop;
+            _currentStop = route.GetNextStopLocation();
+            //todo: calculate the time between stops
+            Debug.Log((_currentStop - _lastStop).magnitude);
+        }
+    }
+}
