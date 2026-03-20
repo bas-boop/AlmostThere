219649dcce52fa7875f8ba476c2665fe4cd3341c
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-03-20 16:24:55 +0100

    Added time to the feeding pigeons event

--- Assets/Scripts/Gameplay/SocialEvents/FeedingPigeons.cs
+++ Assets/Scripts/Gameplay/SocialEvents/FeedingPigeons.cs
@@ -1,14 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
 
+using Framework.FollowMovement;
 using Framework.InteractSystem;
 
 namespace Gameplay.SocialEvents
 {
     public sealed class FeedingPigeons : Interactable
     {
+        [SerializeField] private Followed player;
+        [SerializeField] private float feedingTime = 1;
+        [SerializeField] private float followTime = 1;
+        [SerializeField] private List<Follower> pigeons;
+        
+        // todo: animations for feeding
+        [SerializeField] private UnityEvent onFeed = new();
+        [SerializeField] private UnityEvent onFeedDone = new();
+        
         public override void Interact()
         {
+            if (!p_canInteract)
+                return;
+
+            StartCoroutine(FeedingSequence());
+        }
+
+        private IEnumerator FeedingSequence()
+        {
+            onFeed?.Invoke();
+            yield return new WaitForSeconds(feedingTime);
             
+            foreach (Follower pigeon in pigeons)
+            {
+                player.AddSegment(pigeon);
+            }
+            
+            Invoke(nameof(UnFollow), followTime);
+            onFeedDone?.Invoke();
+        }
+
+        private void UnFollow()
+        {
+            for (int i = pigeons.Count - 1; i >= 0; i--)
+            {
+                Follower pigeon = pigeons[i];
+                player.RemoveSegment(pigeon); // BUG: turn off gameobject?
+                pigeon.gameObject.SetActive(true);
+                // todo: pigeon fly away?
+            }
         }
     }
 }
