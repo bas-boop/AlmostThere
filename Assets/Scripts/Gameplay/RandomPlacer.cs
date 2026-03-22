3f961d06cb9e8b4d5ea95bd1a5a8271e30952121
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-03-22 17:25:26 +0100

    Added randommized goal position

---
+++ Assets/Scripts/Gameplay/RandomPlacer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using Framework.Extensions;
+
+namespace Gameplay
+{
+    public sealed class RandomPlacer : MonoBehaviour
+    {
+        [SerializeField] private GameObject objectToPlace;
+        [SerializeField] private Transform[] locations;
+
+        private void Start() => objectToPlace.transform.position = CollectionExtensions.GetRandomItem(locations).position;
+    }
+}
