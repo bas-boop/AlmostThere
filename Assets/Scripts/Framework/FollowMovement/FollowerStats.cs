8f95910c2c2161845c030a0644d99bf8f7579507
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2026-04-02 14:25:36 +0200

    Added AB diffreses

--- Assets/Scripts/Framework/FollowMovement/FollowerStats.cs
+++ Assets/Scripts/Framework/FollowMovement/FollowerStats.cs
@@ -8,11 +8,13 @@ namespace Framework.FollowMovement
     {
         [Range(0.1f, 25)] public float followDistance;
         [Range(0.1f, 1)] public float smoothSpeed;
+        [Range(0.1f, 5)] public float rotationSpeed;
 
-        private FollowerStats(float? a, float? b)
+        private FollowerStats(float? a, float? b, float? c)
         {
             followDistance = a ?? 0.5f;
             smoothSpeed = b ?? 1f;
+            rotationSpeed = c ?? 5f;
         }
     }
 }
